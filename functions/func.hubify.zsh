85078b047f38754d161579ac0c719f34204d81d9
Bailey Cosier <bailey.cosier@gmail.com>
2015-09-02 17:00:28 +0700

    Updating backup cmds

--- functions/func.hubify.zsh
+++ functions/func.hubify.zsh
@@ -4,4 +4,6 @@ function deploy-hubify(){
   git push origin master
 }
 
-
+function hubify-backup-s3(){
+  s3cmd --recursive cp s3://assets.hubify.com s3://hubify-backups/$(date +%-m_%-d_%Y_%I_%M)/ --access_key=AKIAI5O7YU4QZ3LU3AXA --secret_key=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
+}
