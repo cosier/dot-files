7aecc8f159d3c7a300070fa8e79ecea0ee821656
Bailey Cosier <bailey.cosier@gmail.com>
2015-09-16 20:49:20 +0700

    ...

--- functions/func.hubify.zsh
+++ functions/func.hubify.zsh
@@ -9,7 +9,7 @@ function hubify-backup-s3(){
 }
 
 function hubify-prod-to-dev(){
-  noglob s3cmd --recursive cp s3://assets.hubify.com/* s3://dev.assets.hubify.com/ \
+  s3cmd --recursive cp s3://assets.hubify.com/ s3://dev.assets.hubify.com/ \
     --access_key=AKIAI5O7YU4QZ3LU3AXA \
     --secret_key=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
 }
