e377334b7fe19f32603cf8d76fda5a177dc4cb34
Bailey Cosier <bailey.cosier@gmail.com>
2015-10-14 15:41:16 +0000

    Auto update...

--- functions/func.hubify.zsh
+++ functions/func.hubify.zsh
@@ -20,7 +20,7 @@ function deploy-hubify-production(){
 }
 
 function hubify-backup-s3(){
-  s3cmd --recursive cp s3://assets.hubify.com s3://hubify-backups/$(date +%-m_%-d_%Y_%I_%M)/ --access_key=AKIAI5O7YU4QZ3LU3AXA --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com s3://backups.hubify.com/$(date +%-m_%-d_%Y_%I_%M)/ --access_key=AKIAI5O7YU4QZ3LU3AXA --secret_key=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
 }
 
 function hubify-backup-s3-no-creds(){
@@ -28,7 +28,51 @@ function hubify-backup-s3-no-creds(){
 }
 
 function hubify-s3-prod-to-dev(){
-  s3cmd --recursive cp s3://assets.hubify.com s3://dev.hubify.com/
+  s3cmd --recursive cp s3://assets.hubify.com/listings s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/images s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/hubs s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/users s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/categories s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/skills s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/attached_files s3://dev.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+}
+
+function hubify-s3-prod-to-staging(){
+  s3cmd --recursive cp s3://assets.hubify.com/listings s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/images s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/hubs s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/users s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/categories s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/skills s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
+  s3cmd --recursive cp s3://assets.hubify.com/attached_files s3://staging.assets.hubify.com/ \
+    --access_key=AKIAI5O7YU4QZ3LU3AXA \
+    --secret_key=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
 }
 
 function hubify-prod-to-dev(){
