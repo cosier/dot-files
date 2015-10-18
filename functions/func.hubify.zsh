e4a145aa3171d453ca279abba5cb4a1316275d3f
Bailey <bailey.cosier@gmail.com>
2015-10-18 19:44:56 +0700

    Auto update...

--- functions/func.hubify.zsh
+++ functions/func.hubify.zsh
@@ -1,6 +1,4 @@
 
-
-
 function deploy-hubify(){
   git push origin master:master
   deploy-hubify-staging
@@ -95,5 +93,7 @@ alias dprod='deploy-hubify-production'
 
 alias hssh='ssh ubuntu@dx.hubify.com -v -i ~/.ssh/hubify_v2_dev.pem'
 alias mhssh='mosh bailey@dx.hubify.com'
+alias ssh-hubify-spot='ssh -i ~/.ssh/hubify_dev_workloads.pem ubuntu@spot.hubify.com'
+alias mosh-hubify-spot='mosh --ssh="ssh -i ~/.ssh/hubify_dev_workloads.pem" ubuntu@spot.hubify.com'
 
 alias unicorn-restart='sudo kill -USR2 $(ps aux | grep unicorn | grep master | awk '{ print $2 }')'
