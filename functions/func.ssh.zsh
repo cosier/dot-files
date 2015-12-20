747d82a13d0467863e44270f9ed17cb622088247
Bailey <bailey.cosier@gmail.com>
2015-12-20 18:37:11 +0700

    Auto update...

--- functions/func.ssh.zsh
+++ functions/func.ssh.zsh
@@ -2,11 +2,14 @@
 
 alias cloud-turbo='gcloud compute --project "amazingbackorders" ssh --zone "asia-east1-b" "turbo"'
 
+alias ssh-sg='ssh ubuntu@sg.voiceofpsychic.com -v -i ~/.ssh/sg.pem'
+
 alias ssh-z1='ssh ubuntu@dx.z1.voiceofpsychic.com -v -i ~/.ssh/z1dev.pem'
 alias ssh-z1-b='ssh bailey@dx.z1.voiceofpsychic.com -v'
 alias mosh-z1='mosh bailey@dx.staging.voiceofpsychic.com'
 alias ssh-vopsy='ssh bailey@dx.staging.voiceofpsychic.com -v'
 alias ssh-vopsy-root='ssh root@dx.staging.voiceofpsychic.com -v'
+alias ssh-proxy-vopsy='ssh bailey@dx.staging.voiceofpsychic.com -v -D8192'
 
 alias ssh-doug='ssh bailey@doug -v'
 alias ssh-uzzy='ssh bailey@uzzy -C'
--- functions/func.vop.zsh
+++ functions/func.vop.zsh
@@ -11,6 +11,20 @@ function build-vop(){
   fi
 }
 
+export VOP_PRODUCTION_DB="postgres://vopsy:michigan007@db.staging.voiceofpsychic.com:5500/vopsy"
+
+function vop-production-console(){
+  DATABASE_URL=$VOP_PRODUCTION_DB rails console
+}
+
+function vop-production-migrate(){
+  DATABASE_URL=$VOP_PRODUCTION_DB rake db:migrate
+}
+
+function vop-production-metal(){
+  DATABASE_URL=$VOP_PRODUCTION_DB vopmetal
+}
+
 alias bv='build-vop'
 alias v='vmp'
 alias zenv='zen ~/Developer/work/vop'
@@ -20,7 +34,9 @@ alias vc='vcprod'
 alias vcprod='~/Developer/work/vop/bin/compose.production.sh'
 alias vcdev='~/Developer/work/vop/bin/compose.development.sh'
 
-alias metal='~/Developer/work/vop/bin/start.metal.sh'
+alias vopmetal='~/Developer/work/vop/bin/start.metal.sh'
+alias metal='vopmetal'
+
 alias vs='~/Developer/work/vop/bin/start.sh'
 # alias vp='~/Developer/work/vop/bin/start.production.sh'
 alias vmstart='~/Developer/work/vop/bin/start.manual.sh'
