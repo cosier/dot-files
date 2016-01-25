5fcf13e2575013c7f293193c1b656dd37c75622b
Bailey <bailey.cosier@gmail.com>
2016-01-25 23:49:43 +0700

    Auto update...

--- functions/func.setup.zsh
+++ functions/func.setup.zsh
@@ -27,6 +27,27 @@ function setup-tmkf() {
   tmux select-window -t 0
 }
 
+function setup-vop() {
+  if [ -n "$1" ]; then
+    setup-dir $1
+  fi
+  setup-clean
+
+  tmux rename-window Server
+
+  tmux new-window -n "        V          "
+  tmux new-window -n "      RSPEC        "
+  tmux new-window -n "     CONSOLE       "
+  tmux new-window -n "    REMOTE [C4]    "
+  tmux new-window -n "    REMOTE [SO]    "
+
+  # tmux select-window -t 2
+  # tmux send-keys -t 2 emacs Space -nw Enter
+
+  tmux select-window -t 0
+  # tmux send-keys vim Enter
+}
+
 function setup-zen() {
   if [ -n "$1" ]; then
     setup-dir $1
@@ -97,22 +118,4 @@ function setup-servers(){
   tmux send-keys     postgres
   tmux new-window -n ELASTIC
   tmux send-keys     elasticsearch
-}
-
-function setup-vop(){
-  setup-clean
-
-  tmux rename-window SERVER
-
-  tmux new-window -n MERCURY
-  tmux send-keys     vop; Space cd Space mercury; Space vim Space . Enter
-
-  tmux new-window -n NGINX
-  tmux send-keys     vop; Space cd Space nginx; Space vim Space . Enter
-
-  tmux new-window -n PUMA
-  tmux send-keys     vop; Space cd Space puma; Space vim Space . Enter
-
-  tmux new-window -n CONSOLE
-  # tmux send-keys     elasticsearch
 }
