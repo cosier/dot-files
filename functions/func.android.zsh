da4ed3063b45a6f7d246015931cf42dda38a44d3
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-06 05:30:38 +0700

    Auto update...

--- functions/func.android.zsh
+++ functions/func.android.zsh
@@ -1,7 +1,15 @@
-android-emu() {
+android-emulator() {
   $ANDROID_HOME/tools/emulator $@
 }
 
-android-emu-default() {
-  android-emu -avd Nexus_5X_API_26_x86
+android-emu() {
+  android-emu -avd nx6_api_25_x86_64
+}
+
+android-emu-x86-64() {
+  android-emu -avd nx6_api_25_x86_64
+}
+
+android-emu-armv7a() {
+  android-emu -avd nx6_api_25_armv7a
 }
---
+++ functions/func.scratch.zsh
@@ -0,0 +1,3 @@
+function build-shell-droid() {
+  /home/bailey/Developer/work/droid/shell/bin/build.sh $@
+}
