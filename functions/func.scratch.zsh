10b6e22e9357900133510b7c8798e2ebd95f0b62
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-10 09:23:59 +0700

    Auto update...

--- functions/func.scratch.zsh
+++ functions/func.scratch.zsh
@@ -1,19 +1,3 @@
-function shell-build() {
-  /home/bailey/Developer/work/kinito/shell/bin/build $@
-}
-
-function shell-install() {
-  /home/bailey/Developer/work/kinito/shell/bin/install $@
-}
-
-function shell-apk() {
-  cd /home/bailey/Developer/work/kinito/shell/app/build/outputs/apk/
-}
-
-function shell() {
-  cd /home/bailey/Developer/work/kinito/shell
-}
-
 function hello() {
-  cd /home/bailey/Developer/work/kinito/examples/hello
+  cd ~/Developer/work/kinito/examples/hello
 }
