fafd6935e4a9c529e2f6c38d2d8dc3d217f59538
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-25 19:53:43 +0700

    [up] auto post-install symlinks

--- install.zsh
+++ install.zsh
@@ -3,22 +3,11 @@
 cd ~/.dot
 
 # Dependency management
-#git submodule update --init --recursive
-
-
 
 # Install/overwrite new symlinks
-if [ ! -f ~/.tmux.conf ]; then
-
-  # ln -s ~/.dot/tmux.conf ~/.tmux.conf
-fi
-
 if [ ! -f ~/.netrc ]; then
   ln -sf ~/.dot/netrc ~/.netrc
 fi
-
-
-#ln -sf ~/.dot/pgpass ~/.pgpass
 
 if [ ! -f ~/.irssi ]; then
   ln -sf ~/.dot/irssi ~/.irssi
@@ -29,12 +18,3 @@ ln -sf ~/.dot/hosts ~/.etchosts
 
 #ln -sf ~/.dot/xmodmap.rc ~/.Xmodmap
 
-mkdir -p ~/.ssh
-cp ~/.dot/ssh/* ~/.ssh/
-
-#cp ~/.dot/tmux/default_theme.sh ~/.dot/tmux/powerline/themes/default.sh
-
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/*
-
-#chmod 600 ~/.pgpass
