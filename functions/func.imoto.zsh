4d9e930359d787acfdade5212fbae3da84dcc62f
Bailey Cosier <bailey@cosier.ca>
2017-12-02 10:21:42 -0800

    frontend webpack changes

--- functions/func.imoto.zsh
+++ functions/func.imoto.zsh
@@ -1,10 +1,28 @@
-imoto-frontend() {
+imoto-cmd() {
   export GOOGLE_OAUTH_KEY="33745555561-4bcqte7r73kuto7f8i4ptdl4u5680spb.apps.googleusercontent.com"
   export GOOGLE_KEY_API="o1SrNafeIWCK7yzdO6ysMf83"
-  export API_URL="http://imoto.cosier.ca/api/graphql"
-  export SERVER_URL="http://imoto.cosier.ca/api"
-  export CLIENT_URL="http://imoto.cosier.ca"
-  npm run production-build
-  npm run production
+  export API_URL="https://imoto.cosier.ca/api/graphql"
+  export SERVER_URL="https://imoto.cosier.ca/api"
+
+  API_URL=$API_URL \
+  CLIENT_URL=$CLIENT_URL \
+  SERVER_URL=$SERVER_URL \
+  GOOGLE_OAUTH_KEY=$GOOGLE_OAUTH_KEY \
+  GOOGLE_KEY_API=$GOOGLE_KEY_API \
+  $@
 }
+
+imoto-frontend() {
+  export APP_ENV=production
+  export CLIENT_URL="https://imoto.cosier.ca"
+  imoto-cmd npm run production-build
+  imoto-cmd npm run production
+}
+
+imoto-frontend-dev() {
+  export CLIENT_URL="https://client.imoto.cosier.ca"
+  export APP_ENV=development
+  imoto-cmd npm run dev
+}
+
 
