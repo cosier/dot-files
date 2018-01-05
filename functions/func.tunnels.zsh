44c2afd069b9dfe46a81567afc391887b3d36f6f
Bailey Cosier <bailey@cosier.ca>
2018-01-05 10:35:06 -0800

    [up] auto update

--- functions/func.tunnels.zsh
+++ functions/func.tunnels.zsh
@@ -3,10 +3,10 @@ function tunnel_imoto() {
 }
 
 function tunnel_os_alpha() {
-  autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 6000:localhost:5000 deploy@openstudios.in -NTv
+  autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 6000:localhost:5000 deploy@dx.openstudios.in -NTv
 }
 
 function tunnel_os_dev() {
-  autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 5000:localhost:5000 deploy@openstudios.in -NTv
+  autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 5000:localhost:80 deploy@dx.openstudios.in -NTv
 }
 
