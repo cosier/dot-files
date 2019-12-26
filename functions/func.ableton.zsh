df4ebe470cbcee69958ec249010e6da9faffc190
Bailey Cosier <bailey.cosier@gmail.com>
2019-12-26 10:50:26 +0700

    Updating Ableton

--- functions/func.ableton.zsh
+++ functions/func.ableton.zsh
@@ -1,1 +1,3 @@
 # alias ableton='wine "/home/bailey/.wine/drive_c/users/Public/Application Data/Ableton/Live 9 Lite/Program/Ableton Live 9 Lite.exe"'
+#
+alias ableton='wine ~/Apps/Live/Program/Ableton\ Live\ 10\ Suite.exe'
