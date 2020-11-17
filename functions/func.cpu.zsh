07f1f21f7345e96f529e6375c61344832b6c95e4
Bailey Cosier <bailey.cosier@gmail.com>
2020-11-18 05:18:46 +0700

    places update

--- functions/func.cpu.zsh
+++ functions/func.cpu.zsh
@@ -1,7 +1,18 @@
 function cpu_low(){
-  sudo cpufreq-set -c 1 -d 1200000
+  #sudo cpufreq-set -c 1 -d 1200000
 }
 
 function cpu_max(){
-  sudo cpufreq-set -c 1 -d 2200000
+ #sudo cpufreq-set -c 1 -d 2200000
+}
+
+function cpu_power(){
+  echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+}
+function cpu_powersave(){
+  echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+}
+
+function cpu_power_check() {
+  cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 }
